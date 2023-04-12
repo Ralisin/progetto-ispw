ee9a7b03fe34d0b6c351586eabf40b22e5fcce89
Ralisin <60442337+Ralisin@users.noreply.github.com>
2023-04-12 15:44:35 +0200

    Reset code

--- src/main/java/it/ralisin/littlefarmers/HelloApplication.java
+++ src/main/java/it/ralisin/littlefarmers/HelloApplication.java
@@ -7,11 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class HelloApplication extends Application {
-    static Logger logger = Logger.getLogger(HelloApplication.class.getName());
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -22,8 +18,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        logger.log(Level.INFO, "Try 1");
         launch();
-        logger.log(Level.INFO, "Try 2");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module it.ralisin.littlefarmers {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.logging;
 
 
     opens it.ralisin.littlefarmers to javafx.fxml;
