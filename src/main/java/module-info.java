da4d3ae379e058721936b2b4d96aefa5422b38a6
Ralisin <60442337+Ralisin@users.noreply.github.com>
2023-04-12 15:14:52 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.ralisin.littlefarmers {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens it.ralisin.littlefarmers to javafx.fxml;
+    exports it.ralisin.littlefarmers;
+}
