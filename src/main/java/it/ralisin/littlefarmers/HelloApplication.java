aaa2ad9df555e1a6750c4d3818ca41c912a3c0ef
Ralisin <60442337+Ralisin@users.noreply.github.com>
2023-04-12 15:39:48 +0200

    Second SonalCloud try

--- src/main/java/it/ralisin/littlefarmers/HelloApplication.java
+++ src/main/java/it/ralisin/littlefarmers/HelloApplication.java
@@ -18,8 +18,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("Try 1");
+        System.err.println("Try 1");
         launch();
-        System.out.println("Try 2");
+        System.err.println("Try 2");
     }
 }
