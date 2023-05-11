1d074deaac1586675944a94f5cb90c382a7a17bc
Ralisin <60442337+Ralisin@users.noreply.github.com>
2023-05-12 00:19:16 +0200

    Add file app.properties

--- src/main/java/it/ralisin/littlefarmers/Main.java
+++ src/main/java/it/ralisin/littlefarmers/Main.java
@@ -5,19 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 public class Main extends Application {
+    static Properties properties;
+    static String propertiesPath = "src/main/resources/it/ralisin/littlefarmers/app.properties";
     public void start(Stage stage) throws IOException {
         String firstPage = "HomeScreen.fxml";
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(firstPage));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Title");
+        stage.setTitle(properties.getProperty("APP_NAME"));
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        properties = new Properties();
+        properties.load(new FileInputStream(propertiesPath));
+
         launch();
     }
 }
