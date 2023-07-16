6446c92d8ba5678b60ef5bb821dac1ca50be8e44
Ralisin <60442337+Ralisin@users.noreply.github.com>
2023-07-17 01:21:41 +0200

    try: check SonarCloud bugs

--- src/main/java/it/ralisin/littlefarmers/dao/queries/CustomerDAO.java
+++ src/main/java/it/ralisin/littlefarmers/dao/queries/CustomerDAO.java
@@ -2,11 +2,13 @@ package it.ralisin.littlefarmers.dao.queries;
 
 import it.ralisin.littlefarmers.dao.ConnectionFactory;
 import it.ralisin.littlefarmers.enums.Regions;
-import it.ralisin.littlefarmers.exeptions.DAOException;
 import it.ralisin.littlefarmers.model.Product;
 import it.ralisin.littlefarmers.model.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,23 +24,32 @@ public class CustomerDAO {
                 "from products join (select * from company where region = ?) as companyParsed " +
                 "on products.companyEmail = companyParsed.email";
 
-        PreparedStatement ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        ps.setString(1, region.getRegion());
-        ResultSet rs = ps.executeQuery();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        try {
+            ps = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ps.setString(1, region.getRegion());
+            rs = ps.executeQuery();
 
-        while(rs.next()) {
-            int productId = rs.getInt("productId");
-            String productName = rs.getString("productName");
-            String productDescription = rs.getString("productDescription");
-            float price = rs.getFloat("price");
-            String category = rs.getString("category");
-            String imageLink = rs.getString("imageLink");
+            while (rs.next()) {
+                int productId = rs.getInt("productId");
+                String productName = rs.getString("productName");
+                String productDescription = rs.getString("productDescription");
+                float price = rs.getFloat("price");
+                String category = rs.getString("category");
+                String imageLink = rs.getString("imageLink");
 
-            productList.add(new Product(productId, productName, productDescription, price, category, imageLink, region));
+                productList.add(new Product(productId, productName, productDescription, price, category, imageLink, region));
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
         }
-
-        rs.close();
-        ps.close();
 
         return productList;
     }
@@ -92,7 +103,7 @@ public class CustomerDAO {
         return affectedRows > 0;
     }
 
-    public static boolean removeFromCart(User user, Product product) throws DAOException, SQLException {
+    public static boolean removeFromCart(User user, Product product) throws SQLException {
         Connection conn = ConnectionFactory.getConnection();
 
         String sql = "delete from cart where productId = ? and customerEmail = ?";
