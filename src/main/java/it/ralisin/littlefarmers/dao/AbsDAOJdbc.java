2c2c57df57ae5cd1777bb29ee36962784ed02d73
Ralisin <60442337+Ralisin@users.noreply.github.com>
2023-08-21 14:25:21 +0200

    fix: sonarcloud code smell

--- src/main/java/it/ralisin/littlefarmers/dao/AbsDAOJdbc.java
+++ src/main/java/it/ralisin/littlefarmers/dao/AbsDAOJdbc.java
@@ -1,6 +1,5 @@
 package it.ralisin.littlefarmers.dao;
 
-import it.ralisin.littlefarmers.dao.queries.ProductsDAOJdbc;
 import it.ralisin.littlefarmers.enums.Regions;
 import it.ralisin.littlefarmers.exeptions.DAOException;
 import it.ralisin.littlefarmers.model.Product;
@@ -11,8 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public abstract class AbsDAOJdbc {
     protected static List<Product> executeQueryAndProcessProducts(Connection conn, String sql, Object... params) throws DAOException, SQLException {
@@ -27,7 +24,7 @@ public abstract class AbsDAOJdbc {
             rs = ps.executeQuery();
 
             while (rs.next()) {
-                Product product = ProductsDAOJdbc.getProductFromResultSet(rs);
+                Product product = getProductFromResultSet(rs, true);
                 productList.add(product);
             }
         } catch (SQLException e) {
@@ -39,26 +36,6 @@ public abstract class AbsDAOJdbc {
         }
 
         return productList;
-    }
-
-    protected static Product getProductFromResultSet(ResultSet rs) throws SQLException {
-        String companyEmail = rs.getString("companyEmail");
-        int productId = rs.getInt("productId");
-        String name = rs.getString("productName");
-        String description = rs.getString("productDescription");
-        float price = rs.getFloat("price");
-        int quantity = 0;
-
-        try { quantity = rs.getInt("quantity"); }
-        catch (SQLException e) {
-            Logger.getAnonymousLogger().log(Level.INFO, e.getMessage());
-        }
-
-        Regions region = Regions.getByRegionString(rs.getString("region"));
-        String category = rs.getString("category");
-        String imageLink = rs.getString("imageLink");
-
-        return new Product(companyEmail, productId, name, description, price, region, category, imageLink, quantity);
     }
 
     protected static Product getProductFromResultSet(ResultSet rs, boolean haveQuantity) throws SQLException {
