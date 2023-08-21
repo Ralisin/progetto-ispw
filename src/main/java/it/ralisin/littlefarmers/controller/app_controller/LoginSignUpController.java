1e9e189907e629f39c276ee87be6ef1102e9ba14
Ralisin <60442337+Ralisin@users.noreply.github.com>
2023-08-21 22:55:26 +0200

    fix: sonarcloud code smell

--- src/main/java/it/ralisin/littlefarmers/controller/app_controller/LoginSignUpController.java
+++ src/main/java/it/ralisin/littlefarmers/controller/app_controller/LoginSignUpController.java
@@ -3,20 +3,12 @@ package it.ralisin.littlefarmers.controller.app_controller;
 import it.ralisin.littlefarmers.beans.LoginCredentialsBean;
 import it.ralisin.littlefarmers.dao.queries.CustomerDAO;
 import it.ralisin.littlefarmers.dao.queries.LoginDAO;
-import it.ralisin.littlefarmers.dao.queries.ProductDAO;
-import it.ralisin.littlefarmers.dao.queries.ProductsDAOFactory;
-import it.ralisin.littlefarmers.enums.Regions;
 import it.ralisin.littlefarmers.exeptions.DAOException;
-import it.ralisin.littlefarmers.model.Product;
 import it.ralisin.littlefarmers.model.User;
 import it.ralisin.littlefarmers.utils.CartManagement;
 import it.ralisin.littlefarmers.utils.SessionManagement;
 
-import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class LoginSignUpController {
     public boolean login(LoginCredentialsBean credentials) throws DAOException, SQLException {
