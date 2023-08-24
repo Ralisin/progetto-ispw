9f79cdf9c6491308259b5f2cbfb9f6f8760cd0e3
Ralisin <60442337+Ralisin@users.noreply.github.com>
2023-08-24 20:13:29 +0200

    fix: remove todo in RegionProductsFilterGraphicControllerGUI

--- src/main/java/it/ralisin/littlefarmers/controller/graphic_controller/RegionProductsFilterGraphicControllerGUI.java
+++ src/main/java/it/ralisin/littlefarmers/controller/graphic_controller/RegionProductsFilterGraphicControllerGUI.java
@@ -1,5 +1,5 @@
 package it.ralisin.littlefarmers.controller.graphic_controller;
 
 public class RegionProductsFilterGraphicControllerGUI extends AbsGraphicController {
-    // TODO
+    // This will be the product filter controller, but will not be implemented
 }
